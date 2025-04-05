18d0028f8ebf479fffb3e7137a1e6b9bd822527a
Bakhtyar <bakhtyaransari1@gmail.com>
2025-04-06 03:20:24 +0530

    db url hide

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./utils/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_BJSqpgNCbo26@ep-square-scene-a8znqeh0-pooler.eastus2.azure.neon.tech/ai-interview-mocker?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   },
 });
